cc01af992ffa87659b15ac00decb7154df634ed6
Eminaelle <coraliedurhone@gmail.com>
2024-02-17 14:43:57 +0100

    Amélioration de certains point de code dans analyse et ajout de Docstrings

--- projetsDapplication/AnalyseDonneesSimple/analyse.py
+++ projetsDapplication/AnalyseDonneesSimple/analyse.py
@@ -27,82 +27,115 @@ def main():
     # Checking if the numer of arguments are ok
     check_arg(sys.argv)
     # Extraction of the data in a list of dictionary
-    person_data = read_csv(sys.argv[1])
+    people_data = read_csv(sys.argv[1])
     # Extraction of the age data to put them in a list 
-    ages = data_extraction(person_data)
+    ages_list = data_extraction(people_data)
     # Calculation of the average
-    print(f"the ages are :{ages}")
-    average = calculate_average(ages)
+    print(f"the ages are :{ages_list}")
+    average = calculate_average(ages_list)
     print(f"The average of the age is {average}")
     # Calculation of the median
-    median = calculate_median(ages)
+    median = calculate_median(ages_list)
     print(f"The median of the age is {median}")
     # Calculation of the mode
-    mode = calculate_mode(ages)
+    mode = calculate_mode(ages_list)
     if not mode:
         print("There isn't a mode")
     else:
         print(f"The age(s) the more frequent(s) is {mode}")
 
-def calculate_mode(list):
-    frequence={}
-    # Calculation of the frequence of each element of the list
-    for element in list:
-        if element in frequence:
-            frequence[element] = int(frequence[element]) + 1
+
+def calculate_mode(age_list):
+    """
+    Calculate the mode(s) of a list of ages.
+    
+    Parameters:
+    age_list (list): A list of integer ages.
+    
+    Returns:
+    list: A list of the most frequent age(s) found in age_list. Returns None if there is no mode.
+    """
+    frequency={}
+    # Calculation of the frequency of each element of the list
+    for element in age_list:
+        if element in frequency:
+            frequency[element] = int(frequency[element]) + 1
         else:
-            frequence[element] = 1
+            frequency[element] = 1
         
-    # Finding the element with the max frequence
+    # Finding the element with the max frequency
     max_val = 0
     max_element = []
-    for element in frequence:
-        # If the element has the highest frequence the list is cleared and the element is added
-        if frequence[element] > max_val:
-            max_val = frequence[element]
+    for element in frequency:
+        # If the element has the highest frequency the list is cleared and the element is added
+        if frequency[element] > max_val:
+            max_val = frequency[element]
             max_element = [element]
-        # If the element has the same frequence of another we add it in the list
-        elif frequence[element] == max_val:
+        # If the element has the same frequency of another we add it in the list
+        elif frequency[element] == max_val:
             max_element.append(element)
     # Verification of the existance of a mode, 
-    # if all the elements have the same frequence there is no mode
-    if len(frequence) == len(max_element):
+    # if all the elements have the same frequency there is no mode
+    if len(frequency) == len(max_element):
         return None
     else:
         return max_element  
 
-def calculate_median(list):
+
+def calculate_median(age_list):
+    """
+    Calculate the median age from a list of ages.
+    
+    Parameters:
+    age_list (list): A list of integer ages, not necessarily sorted.
+    
+    Returns:
+    float or int: The median age. If age_list has an even number of elements, returns the average of the two central elements.
+    """
     # Tri de la liste avec la fonction sort_heapsort qui modifie une liste et ne renvoie rien.
-    sort_heapsort(list)
-    if len(list) % 2 == 0:
+    sort_heapsort(age_list)
+    if len(age_list) % 2 == 0:
         # If the list is even the median is egal of the average of the 2 central value
-        return (list[len(list)//2-1] + list[len(list)//2]) / 2
+        return (age_list[len(age_list)//2-1] + age_list[len(age_list)//2]) / 2
     else:
         # If the list is odd the median is egal of the central value
-        return list[len(list) // 2 ]
+        return age_list[len(age_list) // 2 ]
+
+def calculate_average(age_list):
+    """
+    Calculate the average age from a list of ages.
     
-def calculate_average(list):
-    # Return the average of a list
-    i = 0
-    total = 0
-    for element in list:
-        total += element
-        i += 1
-    if i!=0:
-        return total/i
+    Parameters:
+    age_list (list): A list of integer ages.
+    
+    Returns:
+    float: The average age. If the list is empty, the function exits the program.
+    """
+    
+    if age_list:
+        return sum(age_list) / len(age_list)
     else:
         print("The data are empty")
         sys.exit()
 
-def data_extraction(Dict):
-    # Take a dictionnary with an "Age" key and return a list with the ages
+
+def data_extraction(people_data):
+    """
+    Extracts age data from a list of dictionaries and returns a list of ages.
+    
+    Parameters:
+    people_data (list of dict): A list of dictionaries, where each dictionary represents a person's data.
+    
+    Returns:
+    list: A list of integer ages extracted from people_data.
+    """
     data = []
-    for person in Dict:
+    for person in people_data:
         try:
             data.append(int(person['Age']))
         except ValueError:
-            print("The data under 'Age' aren't integer")
-            sys.exit()
+            print("The data under 'Age' aren't integer, please give in arguments a file .csv with Ages")
+            sys.exit(1)
     return data
 
 def check_arg(argv):
@@ -110,12 +143,28 @@ def check_arg(argv):
         print("Put the name of the data file in arguments")
         sys.exit()
 
-def read_csv(arg_file):
-    # Take a file and return a List of Dictionary
-    with open(arg_file, 'r') as file:
-        data = []
-        for row in csv.DictReader(file):
-            data.append(row)
-        return data
 
+def read_csv(file_path):  
+    """
+    Reads a CSV file and returns a list of dictionaries, each representing a row from the file.
+    
+    Parameters:
+    file_path (str): The file path to the CSV file.
+    
+    Returns:
+    list of dict: A list of dictionaries where each key is a column header from the CSV and each value is the corresponding entry for that column.
+    """ 
+    try:
+        with open(file_path, 'r') as file:
+            data = [row for row in csv.DictReader(file)]
+            return data
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found")
+        sys.exit(1)
+    except PermissionError:
+        print(f"Error: Permission denied to read the file '{file_path}'.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        sys.exit(1)
 main()
