42d31c451fcbde5234d740c3f1fe45f4bd855210
Eminaelle <coraliedurhone@gmail.com>
2024-02-21 16:12:52 +0100

    travail sur les fichiers (petits codes d'apprentissage)

---
+++ premiersProjets/names.py
@@ -0,0 +1,12 @@
+names = []
+
+with open("name.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+"""
+with open("name.txt", "r") as file:
+    for line in file:
+        print("hello,", line.rstrip())
+"""
+for name in sorted(names):
+    print(f"hello, {name}")
